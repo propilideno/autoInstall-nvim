1c59a89aabbe13d3e2cce05af0ec0cb2fa7a90c2
Lucas <propilideno@gmail.com>
2023-06-08 14:23:48 -0300

    added auto install menu with stable and nightly builds

--- install.sh
+++ install.sh
@@ -1,11 +1,64 @@
 #!/bin/bash
 
-function install_debian(){
+function error_input(){
+	echo "<WRONG INPUT> $1 is not a valid input!"
+}
+
+function appImage_install_debian(){
     local url_stable="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
     local url_nightly="https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
-    curl -sL $url_stable -o nvim
+	eval "curl -sL \$url_$1 -o nvim"
 	chmod u+x nvim
 	sudo mv nvim /bin/
 }
 
-install_debian
+function show_version(){
+	echo "Do you want show version? (y/n): "
+	read -r choice
+	case "$choice" in
+		'y'|'Y')
+			/bin/nvim --version
+		;;
+	esac
+}
+
+function greetings(){
+	printf "\n<SUCCESS> The installation is complete!\n"
+	echo "==> Contribute with us giving this repo a Star ⭐"
+	echo "Maintainers:"
+	printf "\t  |        <NAME>        |   <GITHUB>   |      <EMAIL>      |\n"
+	printf "\t  | Lucas R. de Almeida  | @propilideno |   hello@propi.dev |\n\n"
+}
+
+# This should be good if you don't have sudo permission on computer
+# Like when you're on college or a public computer
+function appImage_install(){
+	while true; do
+		echo "S - Stable"
+		echo "N - Nightly"
+		echo "What version do you want install? (s/n): "
+		read -r choice
+		case "$choice" in
+			's'|'S')
+				appImage_install_debian "stable"
+				break
+			;;
+			'n'|'N')
+				appImage_install_debian "nightly"
+				break
+			;;
+			*)
+				error_input "$choice"
+			;;
+		esac
+	done
+}
+
+function main(){
+	echo "== Auto Install NVIM =="
+	appImage_install
+	greetings
+	show_version
+}
+
+main
